13a77b4ea3c5e25a3064b580f952e605a680f892
Kateryna Karpova <katyakarpova@gmail.com>
2021-07-15 23:51:12 +0200

    Подготовка проекта

--- src/index.js
+++ src/index.js
@@ -1,3 +1,72 @@
 import './css/styles.css';
+import countryCardTpl from './templates/country.hbs';
+import countriesTpl from './templates/countries.hbs';
+
+const refs = {
+    countryCard: document.querySelector('.country-info'),
+};
 
 const DEBOUNCE_DELAY = 300;
+
+fetchCountries('do');
+
+function fetchCountries(name) {
+    console.log(name);
+    return fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(response => {
+        return response.json();
+    })
+        .then(countries => {
+
+        if (countries.length === 1) {
+            renderOneCountryCardMarkup(countries);
+        }
+        
+        else if (countries.length > 1 || countries.length < 10) {
+            renderCountryCardsMarkup(countries);
+        }
+
+        else {
+            console.log('error');
+        };
+    
+    }).catch(error => {
+        console.log(error);
+    });
+};
+
+function createCountryCardsMarkup(countries) {
+    return countries.map(countriesTpl).join('');
+};
+
+function renderCountryCardsMarkup(countries) {
+    const markup = createCountryCardsMarkup(countries);
+    createCountryCardsMarkup(countries);
+    refs.countryCard.innerHTML = markup;
+};
+
+function renderOneCountryCardMarkup(countries) {
+    const markup = countryCardTpl(...countries);
+    refs.countryCard.innerHTML = markup;
+};
+
+
+
+// fetch('https://restcountries.eu/rest/v2/name/swit').then(response => {
+//     return response.json();
+// }).then(countries => {
+
+//     if (countries.length === 1) {
+//         renderOneCountryCardMarkup(countries);
+//     }
+    
+//     else if (countries.length > 1 || countries.length < 10) {
+//         renderCountryCardsMarkup(countries);
+//     }
+
+//     else {
+//         console.log('error');
+//     };
+    
+// }).catch(error => {
+//     console.log(error);
+// });

5e15f2f365e6a7490814d3499d5cde7772b87d59
Kateryna Karpova <katyakarpova@gmail.com>
2021-07-17 15:05:24 +0200

    Правка разметки
    Исправила шаблоны, добавила очистку разметки

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import './css/styles.css';
 import debounce from 'lodash.debounce';
 import countryCardTpl from './templates/country.hbs';
 import countriesTpl from './templates/countries.hbs';
+//import {fetchCountries} from './fetchCountries';
+
 
 const refs = {
     countryCard: document.querySelector('.country-info'),
+    countryList: document.querySelector('.country-list'),
     input: document.querySelector('#search-box'),
 };
 
@@ -14,49 +17,65 @@ refs.input.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
 
 function onSearch(event) {
     const searchValue = event.target.value;
-    fetchCountries(searchValue);
+    console.log(searchValue);
+    
+        fetchCountries(searchValue)
+            .then(countries => {
+
+                if (countries.length === 1) {
+                    renderOneCountryCardMarkup(countries);
+                }
+        
+                else if (countries.length > 1 && countries.length < 10) {
+                    renderCountryCardsMarkup(countries);
+                }
+
+                else {
+                    console.log('error');
+                };
+    
+            }).catch(error => {
+                console.log('catch');
+                clearMarkup();
+            });
+    
 };
 
 function fetchCountries(name) {
-    return fetch(`https://restcountries.eu/rest/v2/name/${name}`).then(response => {
-        if (!response.ok) {
-            console.log('ups');
-            throw new Error(response.status);
-        }
-        return response.json();
-    })
-        .then(countries => {
-
-        if (countries.length === 1) {
-            renderOneCountryCardMarkup(countries);
-        }
-        
-        else if (countries.length > 1 || countries.length < 10) {
-            renderCountryCardsMarkup(countries);
-        }
-
-        else {
-            console.log('error');
-        };
+    const url = `https://restcountries.eu/rest/v2/name/${name}`
+    return fetch(url)
+        .then(response => {
+            if (!response.ok) {
+                console.log('ups');
+                throw new Error(response.status);
+            }
+            return response.json();
+        });
     
-    }).catch(error => {
-        console.log('catch');
-    });
+};
+
+function renderCountryCardsMarkup(countries) {
+    clearMarkup();
+    const markup = createCountryCardsMarkup(countries);
+    createCountryCardsMarkup(countries);
+    refs.countryList.innerHTML = markup;
+};
+
+function renderOneCountryCardMarkup(countries) {
+    clearMarkup();
+    const markup = countryCardTpl(...countries);
+    refs.countryCard.innerHTML = markup;
 };
 
 function createCountryCardsMarkup(countries) {
     return countries.map(countriesTpl).join('');
 };
 
-function renderCountryCardsMarkup(countries) {
-    const markup = createCountryCardsMarkup(countries);
-    createCountryCardsMarkup(countries);
-    refs.countryCard.innerHTML = markup;
-};
+function clearMarkup() {
+    refs.countryList.innerHTML = "";
+    refs.countryCard.innerHTML = "";
+}
 
-function renderOneCountryCardMarkup(countries) {
-    const markup = countryCardTpl(...countries);
-    refs.countryCard.innerHTML = markup;
-};
+
 
 
